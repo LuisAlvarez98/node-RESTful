44587bb0abf9fbf8fd4fc13d26a4f3272c711827
LuisAlvarez98 <luisfeal1998@hotmail.com>
2020-04-07 22:43:13 +0200

    Mongo DB exercises

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -28,15 +28,47 @@ console.log(result);
 }
 
 async function getCourses(){
-  const courses = await 
-    Course.find({author: 'Luis', isPublished: true})
+  //Comparison Operators
+  // eq =
+  // ne !=
+  // gt >
+  // gte >=
+  // lt <
+  // lte <=
+  // in
+  // nin
+  //Ex. Comparison Operators
+  //.find({price: {$gte: 10, $lte: 20} })
+  //.find({ price: {$in: [10, 15, 20]} })
+
+  //Logical operators
+  // or
+  // and
+  //.find()
+  //.or([{author:'Mosh'}, {isPublished: true}])
+
+   
+
+  //Regular expresions
+  // Starts with Luis RE
+  // .find({ author: /^Luis/ })
+  // //end with alvarez
+  // .find({author: /Alvarez$/i})
+  // //contains Luis
+  // .find({author: /.*Luis.*/i})
+
+  const pageNumber = 2; // pagination
+  const pageSize = 10;  // pagination
+
+  const courses = await Course
+    .find({author: 'Luis', isPublished: true})
+   // .skip((pageNumber - 1) * pageSize) --- pagination
+   // .limit(pageSize) -- pagination
     .limit(10)
     .sort({name: 1})
     .select({name: 1, tags: 1});
+    //.count(); count documents in mongodb
   console.log(courses);
 }
 
-
-
 getCourses();
-
